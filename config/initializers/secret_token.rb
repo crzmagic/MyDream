bff3fdb5e6c2b99ae7c6769700a00a32dabb91c4
crzmagic <crzmagic@gmail.com>
2010-11-25 22:52:01 +0800

    Initialization version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MyDream::Application.config.secret_token = 'fd1c13cb1ffdb520ab14249bf53efb1ca083b52fcca2e590779d309a8c1ac8e342357e60f1d0d25b3ca15855369e5c4c3c1aa8ceedc50ab95c63c24b74c519de'
