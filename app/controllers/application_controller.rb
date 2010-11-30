a299da94c6e55a58d4571f146f5b985b069624cc
crzmagic <crzmagic@gmail.com>
2010-11-30 23:30:44 +0800

    核心功能理解有了大概的轮廓，继续努力!!

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
 class ApplicationController < ActionController::Base
-  protect_from_forgery
+  helper :all # include all helpers, all the time
+  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+#  def set_charset
+#    @headers["Content-Type"] = "text/html; charset=utf-8"
+#  end
+
+  
+  # Scrub sensitive parameters from your log
+  # filter_parameter_logging :password
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
+# Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def title_class(index)
+    current = -1
+    controller = request.path_parameters[:controller]
+    action = request.path_parameters[:action]
+    
+    if controller == "home" and action == "myself" then
+      current = 1
+    end
+    
+    if index == current then
+      return "current"
+    else
+      return ""
+    end
+  end
 end
